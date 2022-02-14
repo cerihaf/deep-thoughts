a6dcd8deedb4f8cf8d38c4e18370e224b5289637
Ceri Trammell <cerihaf@gmail.com>
2022-02-14 14:01:20 -0600

    added profile access

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -13,16 +16,23 @@ const Login = (props) => {
     });
   };
 
+  const [login, { error }] = useMutation(LOGIN_USER);
+
   // submit form
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+    
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }    
   };
+
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +63,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
